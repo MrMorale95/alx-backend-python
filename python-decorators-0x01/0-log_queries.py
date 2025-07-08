c72473791e9e428976eaa390800d44bb5c92a2be
Franklin Zyambo <frankzyambo95@gmail.com>
2025-07-08 09:46:15 +0200

    decorators

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,25 @@
+import sqlite3
+from functools import wraps
+
+def log_queries():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            query = kwargs.get('query') or (args[0] if args else '')
+            print(f"[LOG] Executing SQL query: {query}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@log_queries()
+def fetch_all_users(query):
+    conn = sqlite3.connect('db.sqlite3')  # You might replace this with Django ORM later
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+
+users = fetch_all_users(query="SELECT * FROM users")
+print(users)
