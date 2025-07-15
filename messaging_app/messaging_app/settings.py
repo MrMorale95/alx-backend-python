c668aa83a9dd1c6b0b41467403381bcf7dd807ed
Franklin Zyambo <frankzyambo95@gmail.com>
2025-07-15 15:17:56 +0200

    chnges

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -60,10 +60,13 @@ INSTALLED_APPS = [
 # Django REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
