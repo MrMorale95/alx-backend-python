b8be9635ab625ad63dbdb16b45dbdd482782c790
Franklin Zyambo <frankzyambo95@gmail.com>
2025-07-15 13:24:22 +0200

    chnges

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+class user(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     # profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
@@ -14,17 +14,17 @@ class User(AbstractUser):
         return self.username
 
 
-class Conversation(models.Model):
-    participants = models.ManyToManyField(User, related_name='conversations')
+class conversation(models.Model):
+    participants = models.ManyToManyField(user, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Conversation {self.id}"
 
 
-class Message(models.Model):
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+class message(models.Model):
+    conversation = models.ForeignKey(conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(user, on_delete=models.CASCADE, related_name='sent_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
