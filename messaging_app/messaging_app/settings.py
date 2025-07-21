9a88b41cc33e5916df29a2e725a9ac1dc6962e32
Franklin Zyambo <frankzyambo95@gmail.com>
2025-07-21 21:11:03 +0200

    auth added

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -71,8 +71,8 @@ REST_FRAMEWORK = {
         'chats.permissions.IsParticipantOfConversation',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
-    'PAGE_SIZE': 20,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,  # 20 messages per page
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
