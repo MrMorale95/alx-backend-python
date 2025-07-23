949983c084c8bb0d2ee87940acb03fd2ee3799c8
Franklin Zyambo <frankzyambo95@gmail.com>
2025-07-23 21:46:19 +0200

    set

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,8 +1,9 @@
 # myapp/middleware.py
-from datetime import datetime
+from datetime import datetime, time
 import logging
 import os
 from django.conf import settings
+from django.http import HttpResponseForbidden
 
 logger = logging.getLogger(__name__)
 
@@ -27,3 +28,29 @@ class RequestLoggingMiddleware:
         # Continue processing the request
         response = self.get_response(request)
         return response
+    
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # Define restricted hours (9PM to 6AM)
+        self.restricted_start = time(21, 0)  # 9 PM
+        self.restricted_end = time(6, 0)    # 6 AM
+
+    def __call__(self, request):
+        from datetime import datetime
+        
+        current_time = datetime.now().time()
+        
+        # Check if current time is within restricted hours
+        if (current_time >= self.restricted_start or 
+            current_time <= self.restricted_end):
+            
+            # Allow access to admin even during restricted hours
+            if not (request.user.is_authenticated and request.user.is_staff):
+                return HttpResponseForbidden(
+                    "<h1>Access Denied</h1>"
+                    "<p>Messaging is only available between 6:00 AM and 9:00 PM</p>"
+                )
+        
+        return self.get_response(request)
