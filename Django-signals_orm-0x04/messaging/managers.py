0fe0e39cd8428c36c140df16a040f6c851b1e067
Franklin Zyambo <frankzyambo95@gmail.com>
2025-08-06 09:49:17 +0200

    docker file

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -6,7 +6,7 @@ class UnreadMessagesManager(models.Manager):
     Custom manager to retrieve unread messages for a specific user.
     Uses .only() to fetch minimal required fields for optimization.
     """
-    def for_user(self, user):
+    def unread_for_user(self, user):
         return self.filter(receiver=user, read=False).only(
             "id", "sender", "content", "timestamp"
         )
